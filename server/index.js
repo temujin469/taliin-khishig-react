d44e00dea4b463200a07a41045cdf2b6f993d597
temujin469 <ttemujin469@gmail.com>
2023-01-04 14:57:51 +0800

    comm

--- server/index.js
+++ server/index.js
@@ -41,7 +41,7 @@ app.use("/api/v1/upload", uploadRoutes);
 app.use(errorHandler);
 
 if (process.env.NODE_ENV == "production") {
-  app.use(express.static(path.resolve(__dirname, "../dist", "assets")));
+  app.use(express.static(path.join(__dirname, "../dist")));
 
   app.get("/*", (req, res) => {
     res.sendFile(path.join(__dirname, "../dist", "index.html"));
