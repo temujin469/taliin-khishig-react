6d26dc76367fdea1315ea7a4273c25ec8286d8da
temujin469 <ttemujin469@gmail.com>
2023-01-07 16:48:15 +0800

    node env edited

--- server/utils/uploadImage.js
+++ server/utils/uploadImage.js
@@ -2,13 +2,9 @@ let cloudinary = require("cloudinary").v2;
 const MyError = require("./myError");
 
 cloudinary.config({
-  // cloud_name: process.env.CLOUDINARY_NAME,
-  // api_key: process.env.CLOUDINARY_API_KEY,
-  // api_secret: process.env.CLOUDINARY_API_SECRET,
-  cloud_name: "dqgg1rnar",
-  api_key: "263926512931731",
-  api_secret: "BpSP9R7cx7z8J8XJ6aNwKd-37QM",
-  secure: true,
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 const options = {
