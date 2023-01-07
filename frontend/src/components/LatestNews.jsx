ec9cfe8735b80e7ae35e87d27076bb27a8883ec3
temujin469 <ttemujin469@gmail.com>
2023-01-07 17:02:31 +0800

    skeleton nemlee

--- frontend/src/components/LatestNews.jsx
+++ frontend/src/components/LatestNews.jsx
@@ -4,6 +4,8 @@ import { BsCalendarDate } from "react-icons/bs";
 // import { Link } from "react-router-dom";
 import moment from "moment";
 import { getLatestNews } from "../api/news";
+import { Alert, Skeleton } from "antd";
+import { Link } from "react-router-dom";
 
 function LatestNews() {
   const { data, isLoading, error } = useQuery(
@@ -19,37 +21,46 @@ function LatestNews() {
         Сүүлийн үеийн мэдээ
       </h3>
       <ul className="space-y-4 max-w-[500px]">
-        {!isLoading && !error ? (
-          allNews.map((news) => (
-            <a
-              href={`/news/${news.slug}`}
-              className="flex gap-3"
-              key={news._id}
-            >
-              <div className="h-[130px] w-[130px] overflow-hidden rounded-lg">
-                <img
-                  src={`/upload/${news.photo}`}
-                  className="h-[130px] w-[130px] object-cover hover:scale-[1.1] duration-200"
-                  alt={news.title}
-                />
-              </div>
+        {isLoading
+          ? Array(3)
+              .fill(null)
+              .map((_, i) => (
+                <div className="flex gap-3" key={i}>
+                  <Skeleton.Image active className="h-[130px] w-[130px]" />
+                  <div className="flex-[1]">
+                    <Skeleton active />
+                  </div>
+                </div>
+              ))
+          : allNews
+          ? allNews.map((news) => (
+              <Link
+                to={`/news/${news.slug}`}
+                className="flex gap-3"
+                key={news._id}
+              >
+                <div className="h-[130px] w-[130px] overflow-hidden rounded-lg">
+                  <img
+                    src={news.photo}
+                    className="h-[130px] w-[130px] object-cover hover:scale-[1.1] duration-200"
+                    alt={news.title}
+                  />
+                </div>
 
-              <div className="flex-[1]">
-                <h3 className="text-base font-medium m-0 max-h-[100px] overflow-hidden">
-                  {news.title.length > 100
-                    ? `${news.title.slice(0, 64)}...`
-                    : news.title}
-                </h3>
-                <p className=" text-gray mt-2 flex items-center gap-1">
-                  <BsCalendarDate />
-                  {moment(news.date).format("MMM Do YY")}
-                </p>
-              </div>
-            </a>
-          ))
-        ) : (
-          <p>Loading...</p>
-        )}
+                <div className="flex-[1]">
+                  <h3 className="text-base font-medium m-0 max-h-[100px] overflow-hidden">
+                    {news.title.length > 100
+                      ? `${news.title.slice(0, 64)}...`
+                      : news.title}
+                  </h3>
+                  <p className=" text-gray mt-2 flex items-center gap-1">
+                    <BsCalendarDate />
+                    {moment(news.date).format("MMM Do YY")}
+                  </p>
+                </div>
+              </Link>
+            ))
+          : error && <Alert message={"Алдаа гарлаа"} type="error" />}
       </ul>
     </div>
   );
